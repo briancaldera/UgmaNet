8d7ffec0e423955a004401c60a0c029d26add22f
Brian Caldera <brian_alex07@outlook.com>
2024-11-15 14:51:04 -0400

    ⌛ Add LoaderScreen

---
+++ lib/visual/Screens/Loader.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoaderScreen extends StatelessWidget {
+  const LoaderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Flex(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          direction: Axis.vertical, children: [
+          Image(image: AssetImage('assets/images/logo/UGMA-LOGO.png'), height: 128, width: 128,),
+          SizedBox(height: 10,),
+          CircularProgressIndicator(),
+        ],),
+      )
+    );
+  }
+}

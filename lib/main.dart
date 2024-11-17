3d4a09c5317d7cee78cf8e1f917eb78dc19b9561
Brian Caldera <brian_alex07@outlook.com>
2024-11-16 23:52:06 -0400

    ⚙️ Setup emulator in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:UgmaNet/visual/Screens/app.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/foundation.dart' show kDebugMode;
 import 'package:flutter/material.dart';
 
@@ -15,9 +16,8 @@ void main() async {
 
   if (kDebugMode) {
     FirebaseAuth.instance.useAuthEmulator('localhost', 9099);
-
-    final firestore = FirebaseFirestore.instance;
-    firestore.useFirestoreEmulator('localhost', 8080);
+    FirebaseFirestore.instance.useFirestoreEmulator('localhost', 8080);
+    FirebaseStorage.instance.useStorageEmulator('localhost', 9199);
     // firestore.settings = firestore.settings.copyWith(persistenceEnabled: false);
   }
   // var logger = Logger();
